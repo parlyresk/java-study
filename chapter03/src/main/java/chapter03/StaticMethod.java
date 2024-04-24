3a6104f0079f3771eafad8677d01d89453c1f4dc
parlyresk <parlyresk325@gmail.com>
2024-04-24 16:09:23 +0900

    static method commit

--- chapter03/src/main/java/chapter03/StaticMethod.java
+++ chapter03/src/main/java/chapter03/StaticMethod.java
@@ -4,6 +4,40 @@ public class StaticMethod {
 	int n;
 	static int m;
 	
+	void f1() {
+		n=10;
+	}
 	
+	void f2() {
+		StaticMethod.m=10;
+		
+		m=20;
+	}
+	
+	void f3() {
+		f2();
+	}
+	
+	void f4() {
+		StaticMethod.s1();
+		s1();
+	}
+	static void s1() {
+		// error: static method에서는 인스턴스 변수 접근 불가
+		// n=10;
+	}
+	
+	static void s2() {
+		// error: static method에서 인스턴스 메소드 접근 불가
+		// f1();
+		
+	}
+	static void s3() {
+		StaticMethod.m=10;
+	}
+	
+	static void s4() {
+		StaticMethod.s1();
+	}
 
 }
