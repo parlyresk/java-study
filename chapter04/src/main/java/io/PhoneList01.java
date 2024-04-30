7e0cad151741ad8c58a0887ca394ea7075cca380
parlyresk <parlyresk325@gmail.com>
2024-04-30 14:23:39 +0900

    io 3

--- chapter04/src/main/java/io/PhoneList01.java
+++ chapter04/src/main/java/io/PhoneList01.java
@@ -1,13 +1,19 @@
 package io;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
 
 public class PhoneList01 {
 
 	public static void main(String[] args) {
+		BufferedReader br=null;
 		
 		try {
 			File file=new File("./phone.txt");
@@ -16,14 +22,62 @@ public class PhoneList01 {
 				System.out.println("file not found");
 				return;
 			}
-			FileInputStream fis=new FileInputStream("./phone.txt");
-			fis.read();
+			System.out.println(" === 파일 정보 === ");
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.length() + "Bytes");
+			Date d = new Date(file.lastModified());
+			String date = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(d);
+			System.out.println(date);
 			
-		} catch (FileNotFoundException e) {
-			System.out.println("file not found: "+e);
-		}catch (IOException e) {
-			System.out.println("file not found: "+e);
-		}
+			
+			System.out.println(" === 전화번호 ===");
+			
+			
+			//1. 기반 스트림
+			FileInputStream fis=new FileInputStream(file);
+			
+			//2. 보조스트림2 (바이트|바이트|바이트 -> 캐릭터)
+			InputStreamReader isr=new InputStreamReader(fis,"utf-8");
+			
+			//3. 보조스트림3 (캐릭터|캐릭터|캐릭터|\n -> "charcharchar"
+			
+			br=new BufferedReader(isr);
+			
+			//4. 처리
+			String line=null;
+			while((line=br.readLine())!=null) {
+				StringTokenizer st=new StringTokenizer(line,"\t ");
+				
+				int index=0;
+				while(st.hasMoreElements()) {
+					String token=st.nextToken();
+					if(index==0) {
+						System.out.print(token + ":");
+					}else if(index==1) {
+						System.out.print(token+"-");
+					}else if(index==2) {
+						System.out.print(token+"-");
+					}else {
+						System.out.println(token);
+					}
+					index++;
+				}
+			}
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("error: "+e);
+		} catch(IOException e) {
+			System.out.println("error: "+e);
+		}finally {
+			try {
+				if(br!=null) {
+					br.close();
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+;		}
 
 	}
 
