66f163fff7bde8dd3c6465eff7373ff3dc27195f
parlyresk <parlyresk325@gmail.com>
2024-05-01 16:57:07 +0900

    echo finish

--- network/src/main/java/echo/EchoClient.java
+++ network/src/main/java/echo/EchoClient.java
@@ -10,58 +10,51 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoClient {
-	
-	private static final String SERVER_IP="127.0.0.1";
-
+	private static final String SERVER_IP = "127.0.0.1";
 	
 	public static void main(String[] args) {
-		Scanner scanner=null;
-		Socket socket=null;
+		Scanner scanner = null;
+		Socket socket = null;
 		try {
+			scanner = new Scanner(System.in);
+			socket = new Socket();
 			
-		scanner=new Scanner(System.in);
-		socket=new Socket();
-		
-		socket.connect(new InetSocketAddress(SERVER_IP,EchoServer.PORT));
-		
-		PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8"),true);
-		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"utf-8"));
-		
-		while(true) {
-			System.out.print(">>");
-			String line=scanner.nextLine();
-			if("exit".equals(line)) {
-				break;
+			socket.connect(new InetSocketAddress(SERVER_IP, EchoServer.PORT));
+			
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			
+			while(true) {
+				System.out.print(">>");
+				String line = scanner.nextLine();
+				if("exit".equals(line)) {
+					break;
 				}
-			
-			pw.println(line);
-			String data=br.readLine();
-			if(data==null) {
-				log("suddenly closed by server");
-				break;
+				
+				pw.println(line);
+				String data = br.readLine(); // blocking
+				if(data == null) {
+					log("suddenly closed by server");
+					break;
+				}
+				System.out.println("<<" + data);
 			}
-			System.out.println("<<"+data);
-			
-			}
-		}catch(IOException e) {
-			log("error: "+e);
-		}finally {
+		} catch(IOException e) {
+			log("error:" + e);
+		} finally {
 			try {
 				scanner.close();
-				if(socket!=null && !socket.isClosed()) {
+				
+				if(socket != null && !socket.isClosed()) {
 					socket.close();
 				}
-				
 			} catch (IOException e) {
-				
 				e.printStackTrace();
 			}
-			
 		}
-
 	}
+	
 	private static void log(String message) {
 		System.out.println("[EchoClient] " + message);
 	}
-
 }
--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -11,86 +11,62 @@ import java.net.Socket;
 import java.net.SocketException;
 
 public class EchoServer {
-	public static final int PORT=6000;
+	public static final int PORT = 6000;
+	
 	public static void main(String[] args) {
-		ServerSocket serverSocket=null;
-		
+		ServerSocket serverSocket = null;
 		
 		try {
-			
-			serverSocket=new ServerSocket();
-			
-			serverSocket.bind(new InetSocketAddress("0.0.0.0",PORT),10);
-			Socket socket = serverSocket.accept(); 
+			serverSocket = new ServerSocket();
+
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT), 10);
+			Socket socket = serverSocket.accept();  // blocking
 			
 			try {
-				InetSocketAddress inetRemoteSocketAddress=(InetSocketAddress) socket.getRemoteSocketAddress();
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
 				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
 				int remotePort = inetRemoteSocketAddress.getPort();
+				log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
 				
-				log("connected by client["+remoteHostAddress+":"+ remotePort +"]");
-				
-				
-				
-				
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8"),true);
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"utf-8"));
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
 				
 				while(true) {
-					String data=br.readLine();
-					
-					if(data==null) {
-						log(" closed by client");
+					String data = br.readLine(); // blocking
+					if(data == null) {
+						log("closed by client");
 						break;
 					}
-					
-					
-					log(" received:" + data);
-					
-					
-					//6 데이터 쓰기
+					log("received:" + data);
 					pw.println(data);
-					
 				}
-				
-			}catch(SocketException e) {
-				log(" suddenly closed:"+e);
-				
-			}
-			
-			catch(IOException e) {
-				log(" error:"+e);
-				
-			}finally {
+			} catch(SocketException e) {
+				log("suddenly closed by client");
+			} catch(IOException e) {
+				log("error:" + e);
+			} finally {
 				try {
-					if(socket!=null && !socket.isClosed()) {
+					if(socket != null && !socket.isClosed()) {
 						socket.close();
 					}
-					
-				}catch(IOException e) {
+				} catch(IOException e) {
 					e.printStackTrace();
 				}
-				
 			}
-			
 		} catch (IOException e) {
-			log(" error: " + e);
+			log("error: " + e);
 		} finally {
 			try {
-				if(serverSocket!=null && !serverSocket.isClosed()) {
+				if(serverSocket != null && !serverSocket.isClosed()) {
 					serverSocket.close();
 				}
 			} catch (IOException e) {
-				
 				e.printStackTrace();
 			}
 		}
-
 	}
+	
 	private static void log(String message) {
 		System.out.println("[EchoServer] " + message);
 	}
-
-	
-
 }
