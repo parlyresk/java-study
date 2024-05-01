5df2975daae66c5da780afb9b16ac2f070ce940d
parlyresk <parlyresk325@gmail.com>
2024-05-01 17:55:16 +0900

    nslookup

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,21 +2,30 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String string = "www.naver.com";
-		InetAddress[] name;
-		try {
-			name = InetAddress.getAllByName(string);
-			for(int i=0;i<name.length;i++) {
-				System.out.println(name[i].getHostAddress());
+		while(true) {
+			System.out.print(">");
+			Scanner scanner=new Scanner(System.in);
+			String string = scanner.next();
+			if("exit".equals(string)) {
+				break;
 			}
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			InetAddress[] name;
+			try {
+				name = InetAddress.getAllByName(string);
+				for(int i=0;i<name.length;i++) {
+					System.out.print(name[i].getHostName());
+					System.out.println(" : " +name[i].getHostAddress());
+				}
+				
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 	}
