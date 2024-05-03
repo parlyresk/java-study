703741051aa4d225cf4241862fc665740b99e0b7
parlyresk <parlyresk325@gmail.com>
2024-05-03 11:22:01 +0900

    tcp socket option

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -17,6 +17,9 @@ public class TCPServer {
 			// 1. Server Socket 생성
 			serverSocket = new ServerSocket();
 			
+			// 1.1 FIN_WAIT2 -> TIME_WAIT 상태에서도 소켓 포트 할당이 가능하도록 하기 위해
+			serverSocket.setReuseAddress(true);
+			
 			// 2. 바인딩(binding)
 			//    Socket에 InetSocketAddress[InetAddress(IPAddress) + Port]를 바인딩 한다.
 			//    IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩 하지 않는다.
@@ -37,7 +40,7 @@ public class TCPServer {
 				OutputStream os = socket.getOutputStream();
 				
 				while(true) {
-					System.out.println("try to read");
+					// System.out.println("try to read");
 					//5. 데이터 읽기
 					byte[] buffer = new byte[256];
 					int readByteCount = is.read(buffer); // blocking
@@ -51,6 +54,13 @@ public class TCPServer {
 					System.out.println("[server] received:" + data);
 					
 					// 6. 데이터 쓰기
+					// SO_TIMEOUT 테스트
+//					try {
+//						Thread.sleep(3000);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+					
 					os.write(data.getBytes("utf-8"));
 				}
 			} catch(SocketException e) {
