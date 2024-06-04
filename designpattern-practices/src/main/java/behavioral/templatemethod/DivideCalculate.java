9f39117e4b35e9fee1eadb4115ed7e53978081f9
parlyresk <parlyresk325@gmail.com>
2024-06-04 14:00:01 +0900

    template method

---
+++ designpattern-practices/src/main/java/behavioral/templatemethod/DivideCalculate.java
@@ -0,0 +1,11 @@
+package behavioral.templatemethod;
+
+public class DivideCalculate extends AbstractCalculate {
+
+	@Override
+	public int calculate(int val1, int val2) {
+		// TODO Auto-generated method stub
+		return val1/val2;
+	}
+
+}
